9078770b9f918cee70cdf0a9d9947812d9f0934f
NK <zzum0701@naver.com>
2022-04-08 09:48:36 +0900

    상속 Car

---
+++ src/TextInheritance02/Car.java
@@ -0,0 +1,15 @@
+package TextInheritance02;
+
+class Car {
+	String carname;				// 차 이름
+	String color = "검정색";	// 색
+	int velocity;
+
+	void speedUp() { 			// 속도 증가하다
+		velocity += 5;
+	}
+
+	void speedDown() { 			// 속도 감소하다
+		velocity -= 5;
+	}
+}
---
+++ src/TextInheritance02/Truck.java
@@ -0,0 +1,6 @@
+package TextInheritance02;
+
+class Truck extends Car {
+	// 멤버 변수나 메소드 정의
+	int ton;
+}
